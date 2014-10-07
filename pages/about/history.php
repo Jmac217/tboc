3cc0e6f014e9bbad8c09e6b19ce37ec55c0c95de
Jmac217 <jordanelder10@gmail.com>
2014-10-07 14:00:28 -0500

    tweaks for IE8

--- pages/about/history.php
+++ pages/about/history.php
@@ -1,18 +1,8 @@
-<?php
-	$OpenPanel = '4';
-	$PageTitle = 'History';
-?>
-<?php // include('i_header.php'); ?>
-<div class="span-19 last">
-  <!--<p><img src="res/History.png" alt="History" width="681" height="62"></p>-->
-	<div class='page_header'>History</div>
-  <img src="res/History.jpg" alt="Bank of Vergennes"class="photo text_shadow page_photo"/>
-	<div class='page_body textblock text_shadow calculated_width'>
-		<p>In 1970, the charter was transferred and established as The Bank of Carbondale, with a Main Street location in downtown Carbondale.  In January 1981, several area businessmen purchased the stock of The Bank of Carbondale, which was previously owned by a group outside of the area.  As a result, we can proudly say that we are locally owned and operated.  We are &ldquo;A  Real Community Bank!&rdquo;   The Bank of Carbondale is committed to serving the banking needs of our community – our customers, our neighbors, our friends.</p>
-		<p>The Bank of Carbondale has continued to grow and serve our customers, with the opening of The Vergennes Banking Center in 1994,  The Murphysboro Banking Center in 1996, and The Carterville Banking Center in 2006.  Most recently, a drive- thru facility was opened in Carbondale in 2010, making a total of five area banking locations.</p>
-		<p>The Bank of Carbondale is known by the slogan &ldquo;Your Hometown Friend&rdquo;.  We invite you to stop by our area banking centers to experience the best in community banking.    </p>
-		<p>The Bank of Carbondale was originally chartered as the Vergennes State Bank in 1919, located on the corner of Hack Avenue and Main Street in Vergennes, Illinois.  </p>
-	</div>
+<div class='page_header'>History</div>
+<img src="res/History.jpg" alt="Bank of Vergennes"class="photo text_shadow page_photo"/>
+<div class='page_body textblock text_shadow calculated_width'>
+	<p>In 1970, the charter was transferred and established as The Bank of Carbondale, with a Main Street location in downtown Carbondale.  In January 1981, several area businessmen purchased the stock of The Bank of Carbondale, which was previously owned by a group outside of the area.  As a result, we can proudly say that we are locally owned and operated.  We are &ldquo;A  Real Community Bank!&rdquo;   The Bank of Carbondale is committed to serving the banking needs of our community – our customers, our neighbors, our friends.</p>
+	<p>The Bank of Carbondale has continued to grow and serve our customers, with the opening of The Vergennes Banking Center in 1994,  The Murphysboro Banking Center in 1996, and The Carterville Banking Center in 2006.  Most recently, a drive- thru facility was opened in Carbondale in 2010, making a total of five area banking locations.</p>
+	<p>The Bank of Carbondale is known by the slogan &ldquo;Your Hometown Friend&rdquo;.  We invite you to stop by our area banking centers to experience the best in community banking.    </p>
+	<p>The Bank of Carbondale was originally chartered as the Vergennes State Bank in 1919, located on the corner of Hack Avenue and Main Street in Vergennes, Illinois.  </p>
 </div>
-  <h3><small></small></h3>
-<?php // include('i_footer.php'); ?>
