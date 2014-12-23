7c1a799a2c133a925b8559ff57bc25819e7473cb
Jmac217 <jordanelder10@gmail.com>
2014-12-23 12:59:27 -0600

    updated locations and cod, added th class

--- pages/personal/cod.php
+++ pages/personal/cod.php
@@ -1,114 +1,173 @@
-<?php
-
-//	File: tboc.com/rates.php - Interest Rates
-//	Pulls rate information from MySQL db: tboccom, table: rates
-
-/*
-	$OpenPanel = '2';
-	$PageTitle = 'Certificates of Deposit';
-*/
-
-include '../../php/connect.php';
-
-//Query for ATMs/Status
-$query = "SELECT description, rate, category, minbal, apy, minopen FROM rates ORDER BY linenum";
-$result = mysql_query($query);
-
-// Query for Thru Date
-// Noticed that this could be a JSON file
-$query2 = "SELECT value FROM settings WHERE name = 'rate_thru_date'";
-$result2 = mysql_query($query2);
-$ThruDate = mysql_fetch_assoc($result2);
-	
-?>
 <div class='page_header'>Current Rates</div>
 <div class='textblock text_shadow'>
 
-<table width="100%" border="0" cellspacing="0" cellpadding="0">
+<table width="100%" border="0" cellspacing="0" cellpadding="0" class='td-border'>
 	<tr>
-		<td valign="top">
-			<h2>Interest Rates effective <?php echo $ThruDate['value']; ?></h2>
-<?php
-		//Display ATM Locations
-		if ($result) {
-			$row = mysql_fetch_assoc($result);
-			echo "
-			<table width='500' class='rates' align='center' cellpadding='2' cellspacing='0' border='0'>
-				<tr>
-					<th width='35%'>Account Type</th>
-					<th width='15%' style='text-align:right'>Min. Balance<br />
-						to Open
-					</th>
-					<th width='15%' style='text-align:right;'>Minimum<br/>
-						Balance to Earn APY
-					</th>
-					<th width='15%' style='text-align:right;'>Interest<br/>
-						Rate
-					</th>
-					<th width='20%' style='text-align:right;'>Annual<br/>
-						Percentage<br/>
-						Yield (APY)
-					</th>
-				</tr>";
-			do {
-				echo "
-				<tr>
-					<td class='Left'>".stripslashes($row['description'])."</td>
-					<td style='text-align:right;'>".stripslashes($row['minopen'])."</td>
-					<td style='text-align:right;'>".stripslashes($row['minbal'])."</td>
-					<td style='text-align:right;'>".stripslashes($row['rate'])."</td>
-					<td style='text-align:right;'>".stripslashes($row['apy'])."</td>
-				</tr>";
-				$row = mysql_fetch_assoc($result);
-				if ($row) {
-					echo "
-				<tr class='shaded'>
-					<td class='Left'>".stripslashes($row['description'])."</td>
-					<td style='text-align:right;'>".stripslashes($row['minopen'])."</td>
-					<td style='text-align:right;'>".stripslashes($row['minbal'])."</td>
-					<td style='text-align:right;'>".stripslashes($row['rate'])."</td>
-					<td style='text-align:right;'>".stripslashes($row['apy'])."</td>
-				</tr>
-					";
-				}
-			}while($row = mysql_fetch_assoc($result));
-		}else{
-			echo "
-				<p>Our rates are temporarily unavailable. Please call 618-549-2181 for information or check back soon.</p>
-			";
-		}
-?>
-				<tr>
-					<td style='padding-top: 15px;' colspan='4'>
-						<table width='100%' border='0' align='center' cellpadding='0' cellspacing='0'>
-							<tr>
-								<th colspan='2'>
-									<div align='center'>Certificates of Deposit Early Withdrawal Penalty </div>
-								</th>
-							</tr>
-							<tr>
-								<td width='50%' align='left' class='Left'>1 &amp; 3 Month CD </td>
-								<td width='50%'>1 Month Interest </td>
-							</tr>
-							<tr>
-								<td width='50%' align='left' class='Left'>6 -11  Month CD </td>
-								<td width='50%'>3 Months Interest </td>
-							</tr>
-							<tr>
-								<td width='50%' align='left' class='Left'>12 Months  or Over </td>
-								<td width='50%'>6 Months Interest </td>
-							</tr>
-						</table>
-					</td>
-				</tr>
-			</table>
-			<div class='job_opening'>
-				<p>*The rates may change after the account is opened.</p>
-				<p>A penalty may be imposed for early withdrawal on CDs.</p>
-				<p>Fees could reduce earnings on the account.</p>
-				<p>Additional disclosure information is available in our Truth in Savings brochure.</p>
-			</div>
-		</td>
+		<th colspan='5'>
+			<h2>Interest Rates effective 12-17-2014</h2>
+		</th>
 	</tr>
-</table>
+	<tr>
+		<td><h3>Account Type</h3></td>
+		<td><h3>Min. Balance to Open</h3></td>
+		<td><h3>Minimum Balance to Earn APY</h3></td>
+		<td><h3>Interest Rate</h3></td>
+		<td><h3>Annual Percentage Yield (APY)</h3></td>
+	</tr>
+	<tr>
+		<td>NOW Checking*</td>
+		<td>$1,000</td>
+		<td>$1,000</td>
+		<td>.10%</td>
+		<td>.10%</td>
+	</tr>
+	<tr>
+		<td>Savings Account*</td>
+		<td>$100</td>
+		<td>$100</td>
+		<td>.25%</td>
+		<td>.25%</td>
+	</tr>
+	<tr>
+		<td>Money Market*</td>
+		<td>$2,500</td>
+		<td>$500</td>
+		<td>.15%</td>
+		<td>.15%</td>
+	</tr>
+	<tr>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>$2,500</td>
+		<td>.15%</td>
+		<td>.15%</td>
+	</tr>
+	<tr>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>$10,000</td>
+		<td>.20%</td>
+		<td>.20%</td>
+	</tr>
+	<tr>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>$25,000+</td>
+		<td>.25%</td>
+		<td>.25%</td>
+	</tr>
+	<tr>
+		<td>Elite Money Market*</td>
+		<td>$5,000</td>
+		<td>$5,000</td>
+		<td>.20%</td>
+		<td>.20%</td>
+	</tr>
+	<tr>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>$10,000</td>
+		<td>.25%</td>
+		<td>.25%</td>
+	</tr>
+	<tr>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>$25,000+</td>
+		<td>.25%</td>
+		<td>.25%</td>
+	</tr>
+	<tr>
+		<th colspan='5'>
+			<h2>Certificates of Deposit</h2>
+		</th>
+	</tr>
+	<tr>
+		<td>6 Month CD</td>
+		<td>$1,000</td>
+		<td>$1,000</td>
+		<td>.20%</td>
+		<td>.20%</td>
+	</tr>
+	<tr>
+		<td>6 Month CD</td>
+		<td>$50,000</td>
+		<td>$50,000</td>
+		<td>.30%</td>
+		<td>.30%</td>
+	</tr>
+	<tr>
+		<td>12 Month CD</td>
+		<td>$1,000</td>
+		<td>$1,000</td>
+		<td>.40%</td>
+		<td>.40%</td>
+	</tr>
+	<tr>
+		<td>12 Month CD</td>
+		<td>$50,000</td>
+		<td>$50,000</td>
+		<td>.50%</td>
+		<td>.50%</td>
+	</tr>
+	<tr>
+		<td>18 Month CD</td>
+		<td>$1,000</td>
+		<td>$1,000</td>
+		<td>.50%</td>
+		<td>.50%</td>
+	</tr>
+	<tr>
+		<td>24 Month CD</td>
+		<td>$1,000</td>
+		<td>$1,000</td>
+		<td>.60%</td>
+		<td>.60%</td>
+	</tr>
+	<tr>
+		<td>30 Month CD</td>
+		<td>$1,000</td>
+		<td>$1,000</td>
+		<td>.85%</td>
+		<td>.85%</td>
+	</tr>
+	<tr>
+		<td>36 Month CD</td>
+		<td>$1,000</td>
+		<td>$1,000</td>
+		<td>.90%</td>
+		<td>.90%</td>
+	</tr>
+	</tr>
+	<tr>
+		<td>48 Month CD</td>
+		<td>$1,000</td>
+		<td>$1,000</td>
+		<td>1.00%</td>
+		<td>1.00%</td>
+	</tr>
+	<tr>
+		<th colspan='5'>
+			<h2>Certificates of Deposit Early Withdrawal Penalty</h2>
+		</th>
+	</tr>
+	<tr>
+		<td colspan='3'>1 &amp; 3 Month CD </td>
+		<td colspan='2'>1 Month Interest </td>
+	</tr>
+	<tr>
+		<td colspan='3'>6 -11  Month CD </td>
+		<td colspan='2'>3 Months Interest </td>
+	</tr>
+	<tr>
+		<td colspan='3'>12 Months  or Over </td>
+		<td colspan='2'>6 Months Interest </td>
+	</tr>
+	</table>
+	<div class='sub_div'>
+		<p>*The rates may change after the account is opened.</p>
+		<p>A penalty may be imposed for early withdrawal on CDs.</p>
+		<p>Fees could reduce earnings on the account.</p>
+		<p>Additional disclosure information is available in our Truth in Savings brochure.</p>
+	</div>
 </div>
