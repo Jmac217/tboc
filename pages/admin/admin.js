8e3af1f4374c70a9de85c727cee76d664162feda
Jmac217 <jordanelder10@gmail.com>
2014-12-29 15:02:02 -0600

    admin tabs and json keys

--- pages/admin/admin.js
+++ pages/admin/admin.js
@@ -1,17 +1,35 @@
+jQuery.ajaxSetup({ cache: false });
+
 $('document').ready(function(){
+	var json;
+	$.ajax({
+		dataType:'json',
+		url:'../../json/tiles.json',
+		success: function(data){json=data;}
+	}).then(function(){
+		var tabs = "";
+		for(var i = 0; i <= Object.keys(json).length-1;i++){
+			var _id = Object.keys(json)[i];
+			var _name = _id.charAt(0).toUpperCase() + _id.slice(1);
+			tabs += "<div class='tab'>"+_name+"</div>";
+		}
+		$('#tabs').html(tabs);
+		$('.tab').click(function(){
+		
+			var _tabText = $(this).text().toLowerCase();
+			
+			$('#pages').html(JSON.stringify(json[_tabText]));
+			
 
+			// this number of class in sequence matches with json number in array
+			// - pull contents of json into scope, and perform load_link() into #pages
 
-	$('#username').click(function(){
-		$('#user_options').animate({width: '250px'}).on('mouseleave', function(){$(this).animate({width: '0px'});});
+			/*** Debug ***/
+			//$('#pages').html($(this).text());
+			//$('#pages').load('test.html');		
+		});
 	});
-	$('#user_options').css('height', $('#pages').css('height'));
 
-	$('.tab').click(function(){
-		// this number of class in sequence matches with json number in array
-		// - pull contents of json into scope, and perform load_link() into #pages
-		/*** Debug ***
-		$('#pages').html($(this).text());
-		*/
-		$('#pages').load('test.html');
-	});
+	$('#user').click(function(){$('#user_options').animate({width:'250px'}).on('mouseleave',function(){$(this).animate({width:'0px'});});});
+	$('#user_options').css('height',$('#pages').css('height'));
 });
