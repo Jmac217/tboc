9787e6b02d8f228b74e9c6e192567ce62bd85832
Jmac217 <jordanelder10@gmail.com>
2015-01-06 16:13:46 -0600

    adding to admin

--- pages/admin/admin.js
+++ pages/admin/admin.js
@@ -1,6 +1,9 @@
 jQuery.ajaxSetup({ cache: false });
 
 $('document').ready(function(){
+	$('.tab').mouseover(function(){
+		$(this).css({cursor:'pointer',background:'-moz-linear-gradient(#7ec0ee, #DDD)'});
+	});
 	var json;
 	$.ajax({
 		dataType:'json',
@@ -12,15 +15,56 @@ $('document').ready(function(){
 			var _id = Object.keys(json)[i];
 			var _name = _id.charAt(0).toUpperCase() + _id.slice(1);
 			tabs += "<div class='tab'>"+_name+"</div>";
+			$('#pages').append("<div class='page "+_name.toLowerCase()+"'>"+_id+"</div>");
 		}
 		$('#tabs').html(tabs);
 		$('.tab').click(function(){
-		
 			var _tabText = $(this).text().toLowerCase();
+			//alert(json[_tabText][0].title);
+			$('.'+_tabText).html(JSON.stringify(json[_tabText])).css({display: 'block'}).siblings('.page').css({display: 'none'});
 			
-			$('#pages').html(JSON.stringify(json[_tabText]));
+			switch(_tabText){
+				case "security":
+					//alert(JSON.stringify(json[_tabText]));
+					var html = "";
+					for(var i = 0; i < json[_tabText].length; i++){
+						html += "<div class='list'><span class='list_title'>"+json[_tabText][i].title+"</span><span class='list_body'>"+json[_tabText][i].body+"</span><span class='list_date'>"+json[_tabText][i].date+"</span><div class='list_color box'><select class='color_select_preset'><option>none...</option></select><div class='color_box list_border_choice' title='List Border'></div><div class='color_box list_background_choice' title='List Background'></div><div class='color_box list_font_choice' title='List Font'></div><input class='list_new_preset' placeholder='New Preset...' type='text'/></div></div>";
+					}
+					html += "<div class='list_pop'>+</div>";
+					$('.'+_tabText).html(html);
+					var _selected = "";
+					var _nth = "";
+					$('.list').mouseover(function(){
+						_nth = $(this).index()+1;
+					}).click(function(){
+						$(this).siblings('.list').children('.list_color').css({display: 'none'});
+						$(this).children('.list_color').css({display: 'block'}).children('.color_box').click(function(){_selected = $(this).attr('class').split(' ')[1];}).ColorPicker({
+							color: '#BBBBBB',
+							onShow: function (colpkr) {
+								$(colpkr).fadeIn(500);
+								return false;
+							},
+							onHide: function (colpkr) {
+								$(colpkr).fadeOut(500);
+								return false;
+							},
+							onChange: function (hsb, hex, rgb) {
+								$('.list:nth-child('+_nth+') .'+_selected).css('backgroundColor', '#' + hex);
+								$('.list:nth-child('+_nth+')').css(
+									{
+										border: "solid 1px "+$('.list:nth-child('+_nth+') .list_border_choice').css('backgroundColor'),
+										background: $('.list:nth-child('+_nth+') .list_background_choice').css('backgroundColor'),
+										color: $('.list:nth-child('+_nth+') .list_font_choice').css('backgroundColor')
+									}
+								);
+								$('.list:nth-child('+_nth+') .list_body').css("border-left", "solid 3px "+$('.list:nth-child('+_nth+') .list_border_choice').css('backgroundColor'));
+							}
+						});
+					});
+				break;
+			}
+			$('.page span').attr('contenteditable', 'true');
 			
-
 			// this number of class in sequence matches with json number in array
 			// - pull contents of json into scope, and perform load_link() into #pages
 
@@ -29,6 +73,5 @@ $('document').ready(function(){
 			//$('#pages').load('test.html');		
 		});
 	});
-
 	$('#user_options').css('height',$('#pages').css('height'));
 });
